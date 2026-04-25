0ff227e29af6801b504f3077e3f4ef5fb387c1b8
SasakiMz <maximechoisay3@gmail.com>
2026-04-25 22:24:42 +0200

    Entrain de revoir les cards artisans de la page home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import "../styles/home.scss"
+
 
 const Home = () => {
     const [topArtisans, setTopArtisans] = useState([]);
@@ -20,32 +22,38 @@ const Home = () => {
     
     return (
         <div>
-            <div className="trouveartisan">
-                <h1>Trouvez son Artisan facilement</h1>
-                <p>découvrez un réseau d'artisans profesionnels de la région</p>
-                <p><button>Découvrir les aritsans</button></p>
+            <div className="trouveartisan py-4">
+                <h1 className ="text-center">Trouvez son Artisan facilement</h1>
+                <p className ="text-center">découvrez un réseau d'artisans profesionnels de la région</p>
+                <p className ="text-center"><button className="btn-hometopmain">Découvrir les artisans</button></p>
             </div>
             <div className="commentmarche">
-                <h2>Comment ça marche ?</h2>
+                <h2 className ="py-3 text-center">Comment ça marche ?</h2>
                 <p>1. Choisissez votre catégorie d’artisan </p>
                 <p>2. Choisissez votre artisan </p>
                 <p>3. Le contacter via formulaire </p>
                 <p>4. Une réponse sera apportée sous 48h </p>
             </div>
-            <div>
-                <h3>Artisans du mois ! </h3>
-                <p>Voici les 3 artisans du mois et mieux noté par la communauté !</p>
+            {/* <div>
+                <h3 className ="text-center">Artisans du mois ! </h3>
+                <p className ="text-center">Voici les 3 artisans du mois, sélectionnés et mieux noté par la communauté !</p>
                 {topArtisans
                 .sort((a, b)=> b.rating - a.rating)
                 .slice(0,3)
                 .map(a =>
-                    <div key={a.id}>
-                        <p>{a.name}</p>
-                        <p>{a.location}</p>
-                        <p>{a.rating}</p>
+                    <div key={a.id} className="card-artisan">
+                        <img src="/" alt="artisan" className="artisan-img"/>
+                        <div className="artisan">
+                            <h5>{a.name}</h5>
+                            <div className="rating">★★★★★</div>
+                        </div>
+                        <p className="job">{a.specialty?.name}</p>
+                        <p className="category">{a.specialty?.Category?.name}</p>
+                        <p className="location">{a.location}</p>
+                        <button className="btn-artisan">Contactez le</button>
                     </div>
                  )}
-            </div>
+            </div> */}
         </div>
     )
 }
